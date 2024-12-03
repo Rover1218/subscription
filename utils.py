e14ad7fe458137a48530642bdf85ab4f11a2eec6
Anindya <86872566+Rover1218@users.noreply.github.com>
2024-12-04 01:33:15 +0530

    Add safe_str_cmp utility function and monkey patch flask_login

---
+++ utils.py
@@ -0,0 +1,15 @@
+def safe_str_cmp(a: str, b: str) -> bool:
+    """
+    A replacement for Werkzeug's removed safe_str_cmp function.
+    Performs a constant-time comparison of two strings.
+    """
+    if len(a) != len(b):
+        return False
+    result = 0
+    for x, y in zip(a, b):
+        result |= ord(x) ^ ord(y)
+    return result == 0
+
+# Monkey patch flask_login
+import flask_login.utils
+flask_login.utils.safe_str_cmp = safe_str_cmp
